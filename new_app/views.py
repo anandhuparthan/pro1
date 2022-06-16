ee8ccd7f231fb9bfb7364985fa5493804a5f7aba
anandhuparthan <107032079+anandhuparthan@users.noreply.github.com>
2022-06-16 13:30:45 -0800

    first commit

---
+++ new_app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+def home(request):
+    return HttpResponse("_____HI____")
+
+
+def index(request):
+    return HttpResponse("User")
+
+
+def page(request):
+    return HttpResponse("Third page")
+def fb(request):
+    return HttpResponse("No link")
