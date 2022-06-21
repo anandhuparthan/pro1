131d7e7e6a2161989275c4312e786b0370da6577
anandhuparthan <107032079+anandhuparthan@users.noreply.github.com>
2022-06-21 11:50:24 -0800

    new changes

--- pro1/settings.py
+++ pro1/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q*qohvwbzx#5-n1totzc86aoa_(wrwe*=8%aa8z^8mgbxn(g5s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
